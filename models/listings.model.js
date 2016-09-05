531ad794eddcb8240a5b665281c8c4aa0a13a958
Vishaal Panchhi <vishaal@shoelace.com>
2016-09-05 17:43:34 -0400

    listing schema change

--- models/listings.model.js
+++ models/listings.model.js
@@ -7,23 +7,13 @@ const ListingSchema = new Schema({
     city: String,
     hood: String, //neighbourhood
     street: String,
-    intx: String, //intersection
     address: String,
     zip: String,
     loc : {
         type: [Number],
         index : '2d'
     },
-    bdgt: {
-        min: {
-            type: Number,
-            default: 0
-        },
-        max: {
-            type: Number,
-            default: 999999999
-        }
-    },
+    price: Number,
     bdrm: Number,
     den: Number,
     bath: Number,
