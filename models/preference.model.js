f14487c93ed2ca0bada5d33279fbdc51ed4f3f3f
Nikhil Bhaskar <nikhil38@gmail.com>
2016-09-05 17:08:14 -0400

    test

--- models/preference.model.js
+++ models/preference.model.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //city, neighbourhood, street, intersection, address, budget, bedrooms, den, bath, amenities, exposure, condo name
-const PreferenceSchema = new Schema({
-});
+const PreferenceSchema = new Schema({});
 
 mongoose.model('Preference', PreferenceSchema);
